f6363e3b49bf63ed2507907aa7c4e4d22933888e
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-24 04:51:42 +0530

    Time: 7 ms (92.82%) | Memory: 35.1 MB (35.61%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -16,8 +16,6 @@ class Solution {
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
         int min = *max_element(weights.begin(), weights.end());
        int max = accumulate(weights.begin(), weights.end(), 0ll);
         while(max > min){
