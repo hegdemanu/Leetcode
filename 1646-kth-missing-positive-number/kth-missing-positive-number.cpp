28f60a80d11e75279cbd06c52a376bc0f1886648
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-25 03:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (27.52%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -8,11 +8,9 @@ public:
         int l = 0;
         while(l <= r){
             int mid = (l  + r) / 2;
-            int midi = arr[mid] - (mid + 1);
-            if (midi < k) l = mid + 1;
+            if ((arr[mid] - (mid + 1)) < k) l = mid + 1;
             else r = mid - 1;
-
-        }
+            }
         
       return (l + k);
     }
