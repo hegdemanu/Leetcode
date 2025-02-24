d6f5f225098e1e3b33466399fcb55fa29ea0761a
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-25 03:58:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (27.52%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        int n = arr.size() - 1;
-        int m = arr[n];
-        if (m == n + 1) return m + k;
-        if (k >= m - n) return (m + k - (m - n ) + 1);
+        int r = arr.size() - 1;
+        // int m = arr[r];
+        // if (m == r + 1) return m + k;
+        // if (k >= m - r) return (m + k - (m - r ) + 1);
         int l = 0;
-        int r = n;
-        int cnt = 0;
-        int mid = (l  + r) / 2;
         while(l <= r){
-            
-            // if (arr[mid] == (mid + 1)) l = mid + 1;// sortrd
-            // else{
-                if(l + 1 + cnt == arr[l]) l++;
-                else{
-                //   int ans = arr[l] - l + 1 - cnt;
-                  cnt ++;// count = 1, kth is 1st,
-                  if (k - cnt == 0) return (l + cnt);
-                  cout << cnt;
-                }
-            // }
-            
+            int mid = (l  + r) / 2;
+            int midi = arr[mid] - (mid + 1);
+            if (midi < k) l = mid + 1;
+            else r = mid - 1;
+
         }
-      return -1;
+        
+      return (l + k);
     }
 };
