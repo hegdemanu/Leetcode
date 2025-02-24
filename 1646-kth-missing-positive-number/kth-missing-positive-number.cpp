b80f57cf931f5ccc34252752d8b367161f1a6979
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-25 04:00:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (9.29%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.cpp
@@ -4,9 +4,9 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         int r = arr.size() - 1;
-        // int m = arr[r];
-        // if (m == r + 1) return m + k;
-        // if (k >= m - r) return (m + k - (m - r ) + 1);
+        int m = arr[r];
+        if (m == r + 1) return m + k;
+        if (k >= m - r) return (m + k - (m - r ) + 1);
         int l = 0;
         while(l <= r){
             int mid = (l  + r) / 2;
