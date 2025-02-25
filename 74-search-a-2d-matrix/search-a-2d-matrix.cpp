2a81d6ed6b7ca30107ed4fa24b6ba4218b85d148
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-26 00:23:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (76.09%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        
-        int lo = 0, hi = n - 1;
-        
-        // Binary Search to find the correct row
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            
-            if (matrix[mid][0] <= target && matrix[mid][m - 1] >= target) {
-                // Target is within this row, apply Binary Search on this row
-                int l = 0, h = m - 1;
-                while (l <= h) {
-                    int midy = l + (h - l) / 2;
-                    if (matrix[mid][midy] == target) return true;
-                    else if (matrix[mid][midy] < target) l = midy + 1;
-                    else h = midy - 1;
+        int n = matrix.size();//rows
+        int m = matrix[0].size();//columns
+        int l = 0;int h = n - 1;//row search
+        while(h >= l){
+            int mid = l + (h - l) / 2;
+            if (matrix[mid][m - 1] == target) return true;
+            else if (matrix[mid][m - 1] < target) l = mid + 1;
+            else{
+                if(matrix[mid][0] == target) return true;
+                else if(matrix[mid][0] > target) h = mid - 1;
+                else{
+                    int lo = 0; int hi = m - 1;// column search
+                    while(hi >= lo){
+                        int midy = lo + (hi - lo) / 2;
+                        if (matrix[mid][midy] == target) return true;
+                        else if (matrix[mid][midy]< target) lo = midy + 1;
+                        else{
+                            hi = midy - 1; 
+                        }
+                    }
+                    return false;          
                 }
-                return false; // If not found within the row
             }
-            else if (matrix[mid][m - 1] < target) lo = mid + 1; // Move to a lower row
-            else hi = mid - 1; // Move to a higher row
+
         }
-        
         return false;
     }
 };
