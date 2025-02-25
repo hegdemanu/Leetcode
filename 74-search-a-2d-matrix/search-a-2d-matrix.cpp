17e6753b78e220ee17aeb4726467005dc0e69063
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-26 00:51:55 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (97.77%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,28 +3,18 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) 
     {
         int n = matrix.size();//rows
+        if (n == 0) return false;
         int m = matrix[0].size();//columns
-        int l = 0;int h = n - 1;//row search
+        if (m == 0) return false;
+        int l = 0;int h = m*n - 1;//row search
+        
         while(h >= l){
             int mid = l + (h - l) / 2;
-            if (matrix[mid][m - 1] == target) return true;
-            else if (matrix[mid][m - 1] < target) l = mid + 1;
-            else{
-                if(matrix[mid][0] == target) return true;
-                else if(matrix[mid][0] > target) h = mid - 1;
-                else{
-                    int lo = 0; int hi = m - 1;// column search
-                    while(hi >= lo){
-                        int midy = lo + (hi - lo) / 2;
-                        if (matrix[mid][midy] == target) return true;
-                        else if (matrix[mid][midy]< target) lo = midy + 1;
-                        else{
-                            hi = midy - 1; 
-                        }
-                    }
-                    return false;          
-                }
-            }
+            int row = mid / m;
+            int col = mid % m;
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] > target) h = mid - 1;
+            else l = mid + 1;
 
         }
         return false;
