834e966eec2a1c530e448365429e20bf5bfe6df7
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-26 00:18:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -4,20 +4,28 @@ public:
     {
         int n = matrix.size();
         int m = matrix[0].size();
-        for (int i = 0; i < n; i++)
-        {
-            if(target <= matrix[i][m - 1])
-            {
-              for (int j = 0; j < m; j++)
-              {
-                if(target == matrix[i][j])
-                {
-                    return true;
+        
+        int lo = 0, hi = n - 1;
+        
+        // Binary Search to find the correct row
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (matrix[mid][0] <= target && matrix[mid][m - 1] >= target) {
+                // Target is within this row, apply Binary Search on this row
+                int l = 0, h = m - 1;
+                while (l <= h) {
+                    int midy = l + (h - l) / 2;
+                    if (matrix[mid][midy] == target) return true;
+                    else if (matrix[mid][midy] < target) l = midy + 1;
+                    else h = midy - 1;
                 }
-              } 
-         
+                return false; // If not found within the row
             }
+            else if (matrix[mid][m - 1] < target) lo = mid + 1; // Move to a lower row
+            else hi = mid - 1; // Move to a higher row
         }
+        
         return false;
     }
 };
