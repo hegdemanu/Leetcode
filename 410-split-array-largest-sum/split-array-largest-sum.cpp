c2cbbe6f17f4538efb5d1ba10452f6432edfa569
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-28 02:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (23.37%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -26,15 +26,15 @@ class Solution
             int r = accumulate(nums.begin(), nums.end(), 0);
             if (k == 1) return r;
             int l = *max_element(nums.begin(), nums.end());
-            while (l <= r)
+            while (l < r)
             {
                 int mid = l + (r - l) / 2;
                 if (canSplit(nums, k, mid, n))
                 {
-                    r = mid - 1;
+                    r = mid;
                 }
                 else l = mid + 1;
             }
-            return l;
+            return r;
         }
 };
