2bb703bb2c03eac977d24d972082663e01ca9a81
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-02-28 02:16:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (84.89%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,31 +1,40 @@
-class Solution {
-    bool canSplit(vector<int>& nums, int k, int mid, int n){
-        int sum = 0; int cnt = 1;
-        for (int i = 0; i < n; i++){
-            if((nums[i] + sum) <= mid){
+class Solution
+{
+    bool canSplit(vector<int> &nums, int k, int mid, int n)
+    {
+        int sum = 0;
+        int cnt = 1;
+        for (int i = 0; i < n; i++)
+        {
+            if ((nums[i] + sum) <= mid)
+            {
                 sum += nums[i];
             }
-            else {
+            else
+            {
                 sum = nums[i];
                 cnt++;
                 if (cnt > k) return false;
             }
-        } 
+        }
         return true;
     }
-public:
-    int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        int r = accumulate(nums.begin(), nums.end(), 0);
-        if (k == 1) return r;
-        int l = *max_element(nums.begin(), nums.end());
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(canSplit(nums, k, mid, n)) {
-                r = mid - 1;
+    public:
+        int splitArray(vector<int> &nums, int k)
+        {
+            int n = nums.size();
+            int r = accumulate(nums.begin(), nums.end(), 0);
+            if (k == 1) return r;
+            int l = *max_element(nums.begin(), nums.end());
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if (canSplit(nums, k, mid, n))
+                {
+                    r = mid - 1;
+                }
+                else l = mid + 1;
             }
-            else l = mid + 1;
+            return l;
         }
-        return l;
-    }
 };
