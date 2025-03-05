effd482a7e44c6552fb584606c1139348c16d4a2
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-03-05 23:18:37 +0530

    Time: 6 ms (94.70%) | Memory: 34.6 MB (99.66%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,35 +1,27 @@
 class Solution {
-    bool helper (vector<int>& weights, int days, int mid){
-        ios_base::sync_with_stdio(false);
-        cin.tie(nullptr);
-        int sum = 0;
-        days--;
-       for( int w : weights){
-        sum += w;
-          if(sum > mid){
-            days --;
-            if(days < 0) break;
-            sum = w;// set to current weight
-          }
-       }
-       if (days >= 0) return 1;
-       else return 0;
-    }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         ios_base::sync_with_stdio(false);
         cin.tie(nullptr);
         int min = *max_element(weights.begin(), weights.end());
-       int max = accumulate(weights.begin(), weights.end(), 0ll);
-        while(max >= min){
-            int mid = min + (max - min) / 2;
-            if(helper(weights, days, mid)){
-                max = mid - 1;
-            }
-            else min = mid + 1;
+        int max = accumulate(weights.begin(), weights.end(), 0ll);
 
+        while (max > min) {
+            int mid = min + (max - min) / 2;
+            int sum = 0, remainingDays = days - 1; // Use a separate counter to avoid modifying `days`
+            
+            for (int w : weights) {
+                sum += w;
+                if (sum > mid) {
+                    remainingDays--;
+                    if (remainingDays < 0) break;
+                    sum = w;
+                }
+            }
+            
+            if (remainingDays >= 0) max = mid;
+            else min = mid + 1;
         }
         return min;
     }
-    
 };
