0a836dc4a9677d528d0dd2eddbf00f3f1adb8f47
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-03-15 08:12:58 +0530

    Time: 7 ms (98.53%) | Memory: 88.2 MB (73.07%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -1,20 +1,21 @@
 class Solution {
-public:
-    int maximumCandies(vector<int>& candies, long long k) {
-        long long total = accumulate(candies.begin(), candies.end(),0ll);
-        if(total < k) return 0;
-        int l = 1, r = *max_element(candies.begin(), candies.end());
-        while (l <= r){
-            int mid = (l + (r-l)/2);
-            long long distro = 0;
-            for(int s : candies){
-                distro += s/mid;
-            }
-            if(distro >= k){
-                l = mid + 1;
-            }
-            else r = mid - 1;
+    bool isPossible(vector<int>& can, long long mid, long long k) {
+        long long total = 0;
+        for(auto x: can) {
+            total += x / mid;
         }
-        return r;
+        return total >= k;
+    }
+public:
+    int maximumCandies(vector<int>& can, long long k) {
+        long long n = can.size(), end = 0, start = 1;
+        for(auto x: can) end += x;
+        end /= k;
+        while(start <= end) {
+            long long mid = start + (end - start) / 2;
+            if(isPossible(can, mid, k)) start = mid + 1;
+            else end = mid - 1;
+        }
+        return end;
     }
 };
