b7ca1824a38525d0f843eaa7e357aff6e467d233
hegdemanu <142514044+hegdemanu@users.noreply.github.com>
2025-03-31 03:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (68.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int size1 = nums1.size();
         int size2 = nums2.size();
-        vector<int> mark(size2, 0);
+        // vector<int> mark(size2, 0);
         vector<int> mark2(size1, -1);
         unordered_map <int,int> map;
         stack<int> st;
@@ -12,18 +12,13 @@ public:
                     st.pop();
                 }
             if(!st.empty() && st.top() > nums2[i]){
-                mark[i] = st.top();
+                map[nums2[i]] = st.top();//
                 }
-            else if(st.empty()) mark[i] = -1;
-            for(int j = size1 - 1; j >= 0; j--){
-                if(nums1[j] == nums2[i]){
-                   mark2[j] = mark[i];
-                }
-            }
+            else if(st.empty()) map[nums2[i]] = -1;
             st.push(nums2[i]);
-        }
-        for(int i = size2 - 1; i >= 0; i--){
-            
+            }
+        for (int i = 0; i < size1; i++) {
+        mark2[i] = map.count(nums1[i]) ? map[nums1[i]] : -1;
         }
         return mark2;
     }
