3bbd392827b95fce43aa934ba823e092f9c318c2
B G Manu <142514044+hegdemanu@users.noreply.github.com>
2025-12-04 23:55:47 +0530

    Time: 7 ms (72.98%) | Memory: 19.6 MB (20.87%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.cpp
@@ -4,15 +4,10 @@ public:
         if (arr.size() <= 2) return arr.size();
         int slow = 2, fast = 2;
     while (fast < arr.size()) {
-        // Process current elements
-        // int current = process(arr[slow], arr[fast]);
-        
-        // Update pointers based on condition
         if ((arr[fast] != arr[slow-2])) {
             arr[slow] = arr[fast];
             slow++;
         }
-        // Fast pointer always moves forward
         fast++;
 }
         return slow;
