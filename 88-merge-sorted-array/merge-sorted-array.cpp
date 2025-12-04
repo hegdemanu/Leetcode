9185bba09641e911a6cb02315ddf986991081233
B G Manu <142514044+hegdemanu@users.noreply.github.com>
2025-12-04 21:38:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (38.77%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int gap = (n + m)/2 + (n+m)%2 ;
-        for (int i = 0; i < n ; i++)
-        {
-            nums1[m + i] = nums2[i];
-        }
-        
-        while(gap > 0)
-        {
-            int l = 0;
-            int r = l + gap;
-            {
-                while(r < n + m)
-                {
-                 if (nums1[l] > nums1[r])
-                 {
-                    swap(nums1[l], nums1[r]);
-                    l++;
-                    r++;
-                 }
-                 else r++, l++ ;
-                }
-                if (gap == 1) break;
-                gap = ((gap)/2) + (gap%2);
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        vector<int> ans;
+        int i = 0;
+        int j = 0;
+        while((i < m) && (j < n)){
+            if(n1[i] < n2[j]){
+                ans.push_back(n1[i]);
+                i++;
+            }
+            else if(n1[i] > n2[j]){
+                ans.push_back(n2[j]);
+                j++;
+            }
+            else if(n1[i] == n2[j]){
+                ans.push_back(n2[j]);
+                ans.push_back(n1[i]);
+                j++;
+                i++;
             }
         }
+        while((i < m)){
+              ans.push_back(n1[i]);
+                i++;
+        }
+         while((j < n)){
+              ans.push_back(n2[j]);
+                j++;
+        }
+        n1 = ans;
     }
 };
