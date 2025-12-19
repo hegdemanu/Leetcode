5d430c511683e52cf3a99eaf70356e0aa908e0b0
B G Manu <142514044+hegdemanu@users.noreply.github.com>
2025-12-19 13:07:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (51.48%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,55 +1,34 @@
 class Solution {
-    int binary_search(int high, int low, int target, vector<int>& nums){
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if (nums[mid] != target){
-                if (nums[mid] > target) high = mid - 1;
-                else low = mid + 1;
-            }
-            else return mid;
-        }
-        return -1;
-    }
-    int left_search(int high, int low, int target, vector<int>& nums){
-       int result = binary_search(high, low, target, nums);
-
-        cout << "res: " << result << endl;
-
-       if (result == -1) return result;
-      else{
-         while((result-1) >=0 && nums [result] == nums[result - 1]){
-        result--;
-       }
-       return result;
-      }
-
-    }
-    int right_search(int high, int low, int target, vector<int>& nums){
-       int result = binary_search(high, low, target, nums);
-       if (result == -1) return result;
-       else {
-        while((result + 1) < nums.size() && nums [result] == nums[result + 1] ){
-        result++;
-       }
-       return result;
-       }
-      
-
-    }
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int high = nums.size() - 1;
-        vector<int> solution = {-1, -1};
-        if (nums.empty()) return {-1, -1};
-        else if (high < 1){
-            if (nums[0] == target) return{0, 0};
-            else return solution;
-        }
-        else{
-            solution[0] = left_search(high, 0, target, nums);
-            solution[1] = right_search(high, 0, target, nums);
-            
+    int firstPos(vector<int>& arr, int target) {
+        int l = 0, r = arr.size() - 1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] >= target) {
+                if (arr[mid] == target) ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
-       return solution;
+        }
+        return ans;
+    }
+
+    int lastPos(vector<int>& arr, int target) {
+        int l = 0, r = arr.size() - 1, ans = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (arr[mid] <= target) {
+                if (arr[mid] == target) ans = mid;
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& arr, int target) {
+        return { firstPos(arr, target), lastPos(arr, target) };
     }
 };
